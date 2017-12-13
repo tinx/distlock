903d9e568da8c825db85029f2e18585f1a4ab518
Andreas Jaekel <jaekel@cablecats.de>
2017-12-13 20:33:01 +0100

    Implemented perform_command(), reformatted source
    The function perform_command() has been implemented, and I had
    a chance to learn about the exec package, passing slices as
    varargs with the "..." operator and about type assertions. Fun.
    
    Also used "go fmt" to style the source before it I write any
    more of it - trying to avoid a future mega commit touching all
    source lines by doing this early in the project.

--- distlock.go
+++ distlock.go
@@ -1,70 +1,103 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
 )
 
 func perform_unlock(lock_name string) {
-    return
+	return
 }
 
 func perform_lock(lock_name string, reason string, timeout int) {
-    return
+	return
 }
 
 func perform_command() int {
-    return 0
+	args := flag.Args()
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, "distlock: cmd execution failed: ", err)
+		return 2
+	}
+	if err := cmd.Wait(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			/* exiterr is now type asserted to be type ExitError */
+			status, ok := exiterr.Sys().(syscall.WaitStatus)
+			if ok {
+				/* status is type asserted to be a WaitStatus */
+				return status.ExitStatus()
+			} else {
+				fmt.Println("distlock: unexpected WaitStatus")
+				return 2
+			}
+		} else {
+			fmt.Println("distlock: unexpected ExitError")
+			return 2
+		}
+	}
+	return 0
 }
 
 func main() {
-    lock_name := flag.String("lock-name", "", "Name of the lock to operate on")
-    op_lock := flag.Bool("lock", false, "Acquire lock and exit")
-    op_unlock := flag.Bool("unlock", false, "Release lock and exit")
-    reason := flag.String("reason", "", "Reason why we perform this operation")
-    no_wait := flag.Bool("nowait", false, "Fail if the lock is busy")
-    timeout := flag.Int("timeout", -1, "Max. no. of secs to wait for the lock")
+	lock_name := flag.String("lock-name", "",
+		"Name of the lock to operate on")
+	op_lock := flag.Bool("lock", false, "Acquire lock and exit")
+	op_unlock := flag.Bool("unlock", false, "Release lock and exit")
+	reason := flag.String("reason", "",
+		"Reason why we perform this operation")
+	no_wait := flag.Bool("nowait", false, "Fail if the lock is busy")
+	timeout := flag.Int("timeout", -1,
+		"Max. no. of secs to wait for the lock")
 
-    flag.Parse()
+	flag.Parse()
 
-    if *lock_name == "" {
-        fmt.Fprintln(os.Stderr, "'lock-name' is a required option.")
-        os.Exit(1)
-    }
-    if *op_lock && *op_unlock {
-        fmt.Fprintln(os.Stderr, "Can't give both 'lock' and 'unlock' options.")
-        os.Exit(1)
-    }
-    if (*op_lock || *op_unlock) && flag.NArg() > 0 {
-        fmt.Fprintln(os.Stderr, "Program args given, but would not execute.")
-        os.Exit(1)
-    }
-    if *no_wait {
-        if *timeout > 0 {
-            fmt.Fprintln(os.Stderr, "Conflicting options -nowait and -timeout.")
-            os.Exit(1)
+	if *lock_name == "" {
+		fmt.Fprintln(os.Stderr, "'lock-name' is a required option.")
+		os.Exit(1)
+	}
+	if *op_lock && *op_unlock {
+		fmt.Fprintln(os.Stderr,
+			"Can't give both 'lock' and 'unlock' options.")
+		os.Exit(1)
+	}
+	if (*op_lock || *op_unlock) && flag.NArg() > 0 {
+		fmt.Fprintln(os.Stderr,
+			"Program args given, but would not execute.")
+		os.Exit(1)
+	}
+	if *no_wait {
+		if *timeout > 0 {
+			fmt.Fprintln(os.Stderr,
+				"Conflicting options -nowait and -timeout.")
+			os.Exit(1)
+		} else {
+			*timeout = 0
+		}
+	}
+	if !*op_lock && !*op_unlock && flag.NArg() == 0 {
+		fmt.Fprintln(os.Stderr, "Missing command to protect with lock")
+		os.Exit(1)
+	}
+
+	if *op_unlock {
+		perform_unlock(*lock_name)
+		os.Exit(0)
 	} else {
-            *timeout = 0
-        }
-    }
-    if !*op_lock && !*op_unlock && flag.NArg() == 0 {
-            fmt.Fprintln(os.Stderr, "Missing command to protect with lock")
-            os.Exit(1)
-    }
-
-    if *op_unlock {
-        perform_unlock(*lock_name)
-        os.Exit(0)
-    } else {
-        perform_lock(*lock_name, *reason, *timeout)
-        if *op_lock {
-            /* we're done */
-            os.Exit(0)
-        } else {
-            rc := perform_command()
-            perform_unlock(*lock_name)
-            os.Exit(rc)
-        }
-    }
+		perform_lock(*lock_name, *reason, *timeout)
+		if *op_lock {
+			/* we're done */
+			os.Exit(0)
+		} else {
+			rc := perform_command()
+			perform_unlock(*lock_name)
+			os.Exit(rc)
+		}
+	}
 }
